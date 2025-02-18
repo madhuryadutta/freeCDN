9088c2155e15bd3a5697d32c8fa639ea25553966
Madhurya Dutta <97863234+madhuryadutta@users.noreply.github.com>
2025-02-19 01:16:00 +0530

    refactor: optimize code restructure function elements

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\File;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
@@ -10,54 +9,50 @@ use Image;
 
 class ImageController extends Controller
 {
-    public function show(Request $request, $domain, $filepath)
+    public function show($domain, $filepath, Request $request)
     {
-        $url = $domain.'/'.$filepath; // Replace with your external file URL
-        // Desired file name
-        $extension = pathinfo($url, PATHINFO_EXTENSION); // Extract extension
-        $filename = md5($url).'.'.$extension; // Example: "3a5b8d9fabc123.png"
-        $path = $filename;
-        $response = Http::get($url);
-        if ($response->successful()) {
-            Storage::put($path, $response->body()); // Save file in storage/app/downloads/
+        $url = "{$domain}/{$filepath}";
+        $extension = pathinfo($url, PATHINFO_EXTENSION);
+        $filename = md5($url).'.'.$extension;
 
+        if (Storage::exists($filename)) {
+            return $this->returnImage($filename, ['freeCDN-Cache' => 'HIT']);
         }
 
-        return $this->applyTransformations($filename, []);
+        $response = Http::get($url);
+        if (! $response->successful()) {
+            return response()->json(['error' => 'Image could not be fetched'], 400);
+        }
+
+        Storage::put($filename, $response->body());
+
+        return $this->applyTransformations($filename, $request->all());
     }
 
-    private function applyTransformations($filename, $params)
+    private function applyTransformations($filename, array $params)
     {
         $imagePath = storage_path("app/{$filename}");
 
-        if (isset($params['h'])) {
-            $width = $params['h'];
-        } else {
-            $width = 800;
-        }
-        if (isset($params['w'])) {
-            $height = $params['w'];
-        } else {
-            $height = 800;
-        }
-        // Get file extension
+        $width = $params['w'] ?? 800;
+        $height = $params['h'] ?? 800;
 
-        $image_normal = Image::make($imagePath)->resize($height, $width);
-        $image_normal = $image_normal->stream();
-        Storage::put($filename, $image_normal->__toString());
+        $image = Image::make($imagePath)->resize($width, $height)->stream();
+        Storage::put($filename, $image->__toString());
 
-        return $this->returnToInternet($filename);
+        return $this->returnImage($filename, ['freeCDN-Cache' => 'MISS']);
     }
 
-    private function returnToInternet($filePath)
+    private function returnImage($filename, $headersArray = [])
     {
-        $imagePath = storage_path("app/{$filePath}");
+        $imagePath = storage_path("app/{$filename}");
+
         if (! file_exists($imagePath)) {
             return response()->json(['error' => 'Image not found'], 404);
         }
 
-        return response()->file($imagePath, [
-            'Cache-Control' => 'public, max-age=31536000',
-        ]);
+        $header = ['Cache-Control' => 'public, max-age=31536000'];
+        $headers = array_merge(['Cache-Control' => 'public, max-age=31536000'], $headersArray);
+
+        return response()->file($imagePath, $headers);
     }
 }
